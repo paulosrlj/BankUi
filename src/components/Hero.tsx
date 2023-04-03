8f22fb5531456537d6ae80c2eb8f3faa2a1e15ef
Paulo <paulo.leite@academico.ifpb.edu.br>
2023-04-03 15:51:56 -0300

    feat: add Hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,54 @@
 import React from 'react'
 
+import styles from '../style'
+
+import { robot, discount } from '../assets'
+import GetStarted from './GetStarted'
+
 function Hero() {
   return (
-    <div>Hero</div>
+    <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
+      <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
+        <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
+          <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
+          <p className={`${styles.paragraph} ml-2`}>
+            <span className="text-white">20%</span> de desconto para uma conta de  {" "}
+            <span className="text-white">1 mês</span>
+          </p>
+        </div>
+
+        <div className="flex flex-row justify-between items-center w-full">
+          <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
+            O sistema de pagamento de<br className="sm:block hidden" />{" "}
+            
+          </h1>
+          <div className="ss:flex hidden md:mr-4 mr-0">
+            <GetStarted />
+          </div>
+        </div>
+
+        <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100.8px] leading-[75px] w-full">
+           próxima <span className="text-gradient">Geração</span>{" "}
+        </h1>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Nossa equipe de especialistas utiliza uma metodologia para identificar os cartões de crédito com maior probabilidade de atender às suas necessidades. Nós examinamos taxas de porcentagem anuais e taxas anuais.
+        </p>
+      </div>
+
+      <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
+        <img src={robot} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
+
+        {/* gradient start */}
+        <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
+        <div className="absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40" />
+        <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
+        {/* gradient end */}
+      </div>
+
+      <div className={`ss:hidden ${styles.flexCenter}`}>
+        <GetStarted />
+      </div>
+    </section>
   )
 }
 
