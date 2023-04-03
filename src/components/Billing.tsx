3e54bf8857357f31e1bdde4b06cd91fd7001a72c
Paulo <paulo.leite@academico.ifpb.edu.br>
2023-04-03 16:03:27 -0300

    feat: add Billing component

--- src/components/Billing.tsx
+++ src/components/Billing.tsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import styles, { layout } from "../style";
+import { apple, bill, google } from "../assets";
 
 function Billing() {
   return (
-    <div>Billing</div>
-  )
+    <section id="product" className={layout.sectionReverse}>
+      <div className={layout.sectionImgReverse}>
+        <img
+          src={bill}
+          alt="billing"
+          className="w-[100%] h-[100%] relative z-[5]"
+        />
+
+        {/* gradient start */}
+        <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
+        <div className="absolute z-[0] w-[50%] h-[50%] -left-1/2 bottom-0 rounded-full pink__gradient" />
+        {/* gradient end */}
+      </div>
+
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          Controle com facilidade <br className="sm:block hidden" /> as suas finanças
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Com o nosso app, você está no total controle das suas finanças, em qualquer lugar.
+        </p>
+
+        <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
+          <img
+            src={apple}
+            alt="google_play"
+            className="w-[128.86px] h-[42.05px] object-contain mr-5 cursor-pointer"
+          />
+          <img
+            src={google}
+            alt="google_play"
+            className="w-[144.17px] h-[43.08px] object-contain cursor-pointer"
+          />
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Billing
+export default Billing;
