d7b9b2002c6dc9fce689ec991f045f6ae8b211ae
Paulo <paulo.leite@academico.ifpb.edu.br>
2023-04-03 16:06:53 -0300

    feat: add Testimonials component

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import styles from "../style";
+import { feedback } from "../constants";
+import FeedbackCard from "./FeedbackCard";
 
 function Testimonials() {
   return (
-    <div>Testimonials</div>
-  )
+    <section
+      id="clients"
+      className={`${styles.paddingY} ${styles.flexCenter} flex-col relative `}
+    >
+      <div className="absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient bottom-40" />
+
+      <div className="w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]">
+        <h2 className={styles.heading2}>
+         O que as pessoas estão <br className="sm:block hidden" />  dizendo de nós
+        </h2>
+        <div className="w-full md:mt-0 mt-6">
+          <p className={`${styles.paragraph} text-left max-w-[450px]`}>
+          Tudo o que você precisa para aceitar pagamentos com cartão e expandir seus negócios em qualquer lugar do planeta.
+          </p>
+        </div>
+      </div>
+
+      <div className="flex flex-wrap sm:justify-start justify-center w-full feedback-container relative z-[1]">
+        {feedback.map((card) => (
+          <FeedbackCard key={card.id} {...card} />
+        ))}
+      </div>
+    </section>
+  );
 }
 
-export default Testimonials
+export default Testimonials;
