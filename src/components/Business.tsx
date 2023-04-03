19d2f1b7343326a43c9f8185af92946f2caf829f
Paulo <paulo.leite@academico.ifpb.edu.br>
2023-04-03 16:01:00 -0300

    feat: add business component

--- src/components/Business.tsx
+++ src/components/Business.tsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import styles, { layout } from "../style";
+import { features } from "../constants";
+import Button from "./Button";
+
+type Props = {
+  icon: string;
+  title: string;
+  content: string;
+  index: number;
+};
+
+const FeatureCard = ({ icon, title, content, index }: Props) => (
+  <div
+    className={`flex flex-row p-6 rounded-[20px] ${
+      index !== features.length - 1 ? "mb-6" : "mb-0"
+    } feature-card`}
+  >
+    <div
+      className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}
+    >
+      <img src={icon} alt="star" className="w-[50%] h-[50%] object-contain" />
+    </div>
+    <div className="flex-1 flex flex-col ml-3">
+      <h4 className="font-poppins font-semibold text-white text-[18px] leading-[23.4px] mb-1">
+        {title}
+      </h4>
+      <p className="font-poppins font-normal text-dimWhite text-[16px] leading-[24px]">
+        {content}
+      </p>
+    </div>
+  </div>
+);
 
 function Business() {
   return (
-    <div>Business</div>
-  )
+    <section id="features" className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          Você faz negócios, <br className="sm:block hidden" /> nós cuidamos do
+          dinheiro.
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Com o cartão de crédito certo, você pode melhorar sua vida financeira
+          acumulando crédito, ganhando recompensas e economizando dinheiro.
+        </p>
+
+        <Button styles={`mt-10`} />
+      </div>
+
+      <div className={`${layout.sectionImg} flex-col`}>
+        {features.map((feature, index) => (
+          <FeatureCard key={feature.id} {...feature} index={index} />
+        ))}
+      </div>
+    </section>
+  );
 }
 
-export default Business
+export default Business;
