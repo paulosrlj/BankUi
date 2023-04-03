8715a1ffa2e2b1237c9c0483733aa1c1a5abba2c
Paulo <paulo.leite@academico.ifpb.edu.br>
2023-04-03 14:38:46 -0300

    feat: add Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,37 +2,37 @@ import { useState } from "react";
 
 import styles from "./style";
 
+import {
+  Billing,
+  Business,
+  CTA,
+  CardDeal,
+  Clients,
+  Footer,
+  Hero,
+  Navbar,
+  Stats,
+  Testimonials,
+} from "./components";
+
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="bg-primary w-full overflow-hidden">
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-        <div className={`${styles.boxWidth}`}>
-          Navbar
-        </div>
+        <div className={`${styles.boxWidth}`}><Navbar /></div>
       </div>
 
-
       <div className={`bg-primary ${styles.flexStart}`}>
-        <div className={`${styles.boxWidth}`}>
-          Hero
-        </div>
+        <div className={`${styles.boxWidth}`}><Hero /></div>
       </div>
 
       <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          Stats
-          Business
-          Billing
-          CardDeal
-          Testimonials
-          Clients
-          CTA
-          Footer
+          <Stats /> <Business /> <Billing /> <CardDeal /> <Testimonials /> <Clients /> <CTA /> <Footer />
         </div>
       </div>
-
     </div>
   );
 }
